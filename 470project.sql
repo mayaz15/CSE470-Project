65446e2f66fc7573ebd773b11acfe26268fa1ce6
mayaz15 <111387484+mayaz15@users.noreply.github.com>
2022-08-18 22:38:29 +0600

    Add files via upload

---
+++ 470project.sql
@@ -0,0 +1,305 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 20, 2022 at 06:55 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `470project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `alogin`
+--
+
+CREATE TABLE `alogin` (
+  `id` int(11) NOT NULL,
+  `email` tinytext NOT NULL,
+  `password` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `alogin`
+--
+
+INSERT INTO `alogin` (`id`, `email`, `password`) VALUES
+(1, 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE `employee` (
+  `id` int(11) NOT NULL,
+  `firstName` varchar(100) NOT NULL,
+  `lastName` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` text NOT NULL,
+  `birthday` date NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  `nid` int(20) NOT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `dept` varchar(100) NOT NULL,
+  `degree` varchar(100) NOT NULL,
+  `pic` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `employee`
+--
+
+INSERT INTO `employee` (`id`, `firstName`, `lastName`, `email`, `password`, `birthday`, `gender`, `contact`, `nid`, `address`, `dept`, `degree`, `pic`) VALUES
+(101, 'Reshad', 'Karim', 'reshad@reshad.corp', '1234', '1999-03-12', 'Male', '01919', 12121, 'Mirpur Dhaka', 'IT', 'Head', '01.jpg'),
+(102, 'Naima ', 'Ahmed', 'naimaahmed@gmail.com', '1234', '2008-01-02', 'Female', '0202', 303, 'Khilgaon Dhaka', 'CSE', 'CSE', '02.png'),
+(103, 'Farnaz', 'Fawad', 'farnaz@xyz.corp', '1234', '1990-02-02', 'Female', '5252', 6262, 'Mirpur-12, UK', 'Creative', 'MSc', '03.jpg'),
+(104, 'Muballigh ', 'hossain', 'muba@xyz.corp', '1234', '1971-12-01', 'Male', '9595', 5959, 'USA', 'Creative', 'MSc', '04.png'),
+(105, 'Navid', 'Mahmud', 'navid@spacex.com', '1234', '1971-06-28', 'Male', '8585', 5858, 'Canada', 'SpaceTech', 'BSc', '05.jpg'),
+(106, 'Juboraj', 'Man', 'Juboraj@xyz.corp', '1234', '1990-02-02', 'Male', '7575', 5757, 'Underground, Dhaka', 'NetworkSecurity', 'MSc', '06.jpg'),
+(107, 'Mashroor ', 'Ahmed', 'mashroor@xyz.corp', '1234', '1993-03-03', 'Male', '4545', 5454, 'USA', 'Defense ', 'MS', '07.png'),
+(108, 'Samanta ', 'Tabassum', 'samanta@xyz.corp', '1234', '1976-04-16', 'Female', '758758', 857857, 'USA', 'AI', 'PhD', '08.png'),
+(109, 'Hisham', 'Abyad', 'hishu@xyz.corp', '1234', '1985-01-01', 'Male', '852852', 258258, 'USA', 'AI', 'PhD', '09.png'),
+(110, 'Mahbub', 'Zaman', 'mahbub@xyz.corp', '1234', '1965-09-18', 'Male', '147147', 741741, 'USA', 'NLP', 'PhD', '10.png'),
+(111, 'Anita', 'Tasnim', 'anita@xyz.corp', '1234', '2005-08-06', 'Female', '0187282', 112233, 'Winterfell', 'Management', 'BSc.', '12.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee_leave`
+--
+
+CREATE TABLE `employee_leave` (
+  `id` int(11) DEFAULT NULL,
+  `token` int(11) NOT NULL,
+  `start` date DEFAULT NULL,
+  `end` date DEFAULT NULL,
+  `reason` char(100) DEFAULT NULL,
+  `status` char(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `employee_leave`
+--
+
+INSERT INTO `employee_leave` (`id`, `token`, `start`, `end`, `reason`, `status`) VALUES
+(101, 301, '2019-04-07', '2019-04-08', 'Sick Leave', 'Approved'),
+(102, 305, '2019-04-07', '2019-04-08', 'Urgent Family Cause', 'Approved'),
+(103, 306, '2019-04-08', '2019-04-08', 'Concert Tour', 'Approved'),
+(101, 307, '2019-04-14', '2019-04-30', 'Want to see GOT', 'Pending'),
+(105, 308, '2019-04-26', '2019-04-30', 'Launching Tesla Model Y', 'Pending'),
+(111, 309, '2019-04-09', '2019-04-13', 'Visit to Kings Landing', 'Pending'),
+(104, 310, '2019-04-08', '2019-04-09', 'Emergency Leave', 'Pending');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project`
+--
+
+CREATE TABLE `project` (
+  `pid` int(11) NOT NULL,
+  `eid` int(11) DEFAULT NULL,
+  `pname` varchar(100) DEFAULT NULL,
+  `duedate` date DEFAULT NULL,
+  `subdate` date DEFAULT '0000-00-00',
+  `mark` int(11) NOT NULL,
+  `status` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `project`
+--
+
+INSERT INTO `project` (`pid`, `eid`, `pname`, `duedate`, `subdate`, `mark`, `status`) VALUES
+(213, 101, 'Database', '2019-04-07', '2019-04-04', 10, 'Submitted'),
+(214, 102, 'Test', '2019-04-10', '0000-00-00', 0, 'Due'),
+(215, 105, 'Tesla Model Y', '2019-04-19', '2019-04-06', 10, 'Submitted'),
+(216, 106, 'Hack', '2019-05-04', '2019-04-05', 5, 'Due'),
+(217, 111, 'Do Nothing', '2019-04-02', '2019-04-01', 8, 'Submitted'),
+(218, 105, 'Tesla Model X', '2019-04-03', '2019-04-03', 10, 'Submitted'),
+(219, 101, 'PHP', '2019-04-07', '0000-00-00', 0, 'Due'),
+(220, 110, 'Data Analysis', '2019-04-16', '2019-04-04', 8, 'Submitted'),
+(221, 110, 'Data Analysis', '2019-04-16', '2019-04-04', 7, 'Submitted'),
+(222, 103, 'Statistical', '2019-04-19', '2019-04-04', 6, 'Due'),
+(223, 108, 'Software Scema', '2019-04-09', '2019-04-02', 3, 'Submitted'),
+(224, 107, 'Security Check', '2019-04-26', '2019-04-05', 9, 'Due'),
+(225, 109, 'ML', '2019-04-03', '2019-04-04', 6, 'Due');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rank`
+--
+
+CREATE TABLE `rank` (
+  `eid` int(11) NOT NULL,
+  `points` int(11) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `rank`
+--
+
+INSERT INTO `rank` (`eid`, `points`) VALUES
+(101, 10),
+(102, 0),
+(103, 6),
+(104, 0),
+(105, 20),
+(106, 5),
+(107, 9),
+(108, 3),
+(109, 6),
+(110, 15),
+(111, 8);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `salary`
+--
+
+CREATE TABLE `salary` (
+  `id` int(11) NOT NULL,
+  `base` int(11) NOT NULL,
+  `bonus` int(11) DEFAULT NULL,
+  `total` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `salary`
+--
+
+INSERT INTO `salary` (`id`, `base`, `bonus`, `total`) VALUES
+(101, 55000, 10, 60500),
+(102, 16500, 0, 16500),
+(103, 65000, 6, 68900),
+(104, 78000, 0, 78000),
+(105, 105000, 20, 126000),
+(106, 60000, 5, 63000),
+(107, 77000, 9, 83930),
+(108, 50000, 3, 51500),
+(109, 85000, 6, 90100),
+(110, 47000, 15, 54050),
+(111, 45000, 8, 48600);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `alogin`
+--
+ALTER TABLE `alogin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `employee_leave`
+--
+ALTER TABLE `employee_leave`
+  ADD PRIMARY KEY (`token`),
+  ADD KEY `employee_leave_ibfk_1` (`id`);
+
+--
+-- Indexes for table `project`
+--
+ALTER TABLE `project`
+  ADD PRIMARY KEY (`pid`),
+  ADD KEY `project_ibfk_1` (`eid`);
+
+--
+-- Indexes for table `rank`
+--
+ALTER TABLE `rank`
+  ADD PRIMARY KEY (`eid`);
+
+--
+-- Indexes for table `salary`
+--
+ALTER TABLE `salary`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `alogin`
+--
+ALTER TABLE `alogin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=112;
+
+--
+-- AUTO_INCREMENT for table `employee_leave`
+--
+ALTER TABLE `employee_leave`
+  MODIFY `token` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=311;
+
+--
+-- AUTO_INCREMENT for table `project`
+--
+ALTER TABLE `project`
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=226;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `employee_leave`
+--
+ALTER TABLE `employee_leave`
+  ADD CONSTRAINT `employee_leave_ibfk_1` FOREIGN KEY (`id`) REFERENCES `employee` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `project`
+--
+ALTER TABLE `project`
+  ADD CONSTRAINT `project_ibfk_1` FOREIGN KEY (`eid`) REFERENCES `employee` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `rank`
+--
+ALTER TABLE `rank`
+  ADD CONSTRAINT `rank_ibfk_1` FOREIGN KEY (`eid`) REFERENCES `employee` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `salary`
+--
+ALTER TABLE `salary`
+  ADD CONSTRAINT `salary_ibfk_1` FOREIGN KEY (`id`) REFERENCES `employee` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
